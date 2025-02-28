201109e16c0f6f3dd0e00118040b6f27c1b7ed06
hemanth-kumar-j <hemanth@nitimis.com>
2025-02-28 19:16:55 +0530

    Added headlesschrome to basic_auth.py

--- basic_auth.py
+++ basic_auth.py
@@ -1,13 +1,29 @@
 import time
+import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Get browser mode from command-line argument (default: "chrome")
+headless = (
+    "--headless=new" if len(sys.argv) > 1 and sys.argv[1].lower() == "headless" else ""
+)
 
 # Credentials
 USERNAME = "admin"
 PASSWORD = "admin"
 URL = f"https://{USERNAME}:{PASSWORD}@the-internet.herokuapp.com/basic_auth"
 
+# Set Chrome options
+options = Options()
+if headless:
+    options.add_argument(headless)
+
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+
 # Initialize WebDriver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
 # Open the webpage with authentication
